4280c365d8cbb88b25a96faeaaad2163a959b6f4
NickyHariniai <ferrissushi@gmail.com>
2026-06-05 15:18:18 +0300

    chore: add user mapping in the service

--- src/main/java/com/ferrissushi/brainfart/service/UserService.java
+++ src/main/java/com/ferrissushi/brainfart/service/UserService.java
@@ -1,8 +1,12 @@
 package com.ferrissushi.brainfart.service;
 
 import com.ferrissushi.brainfart.model.User;
+import com.ferrissushi.brainfart.model.dto.CreateUserDto;
 import com.ferrissushi.brainfart.repository.UserRepository;
+
+import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,7 +21,25 @@ public class UserService {
     return userRepository.findAll();
   }
 
-  public User save(User user) {
+  public User save(CreateUserDto request) {
+    var user =
+        User.builder()
+            .id(UUID.randomUUID().toString())
+            .name(request.name())
+            .username(request.username())
+            .email(request.email())
+            .image(request.image())
+            .rank(0)
+            .banned(false)
+            .role(User.Role.USER)
+            .banReason(null)
+            .emailVerified(false)
+            .displayUsername(request.username())
+            .createdAt(Instant.now())
+            .updatedAt(Instant.now())
+            .highestScore(0)
+            .banExpires(null)
+            .build();
     return userRepository.save(user);
   }
 }
