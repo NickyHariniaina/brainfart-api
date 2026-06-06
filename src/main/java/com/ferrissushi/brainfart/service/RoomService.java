4f04222835d503991f10e0b8d3deb2c764719953
NickyHariniai <ferrissushi@gmail.com>
2026-06-06 10:21:03 +0300

    chore: search for question type before creation

--- src/main/java/com/ferrissushi/brainfart/service/RoomService.java
+++ src/main/java/com/ferrissushi/brainfart/service/RoomService.java
@@ -4,6 +4,7 @@ import com.ferrissushi.brainfart.model.Room;
 import com.ferrissushi.brainfart.model.User;
 import com.ferrissushi.brainfart.model.dto.CreateRoomDto;
 import com.ferrissushi.brainfart.model.dto.RoomDto;
+import com.ferrissushi.brainfart.repository.QuestionTypeRepository;
 import com.ferrissushi.brainfart.repository.RoomRepository;
 import com.ferrissushi.brainfart.repository.UserRepository;
 import java.util.List;
@@ -17,14 +18,19 @@ import org.springframework.stereotype.Service;
 public class RoomService {
   private final RoomRepository roomRepository;
   private final UserRepository userRepository;
+  private final QuestionTypeRepository questionTypeRepository;
 
   public List<RoomDto> findAll() {
     return roomRepository.findAll().stream().map(RoomDto::from).toList();
   }
 
   public Room save(CreateRoomDto request) {
+    var questionType =
+        questionTypeRepository
+            .findByName(request.questionTypeName())
+            .orElseThrow(() -> new RuntimeException("Type not found"));
     var user = userRepository.findByUsername("ferrissushi").orElse(User.builder().build());
-    var room = request.toRoom(user);
+    var room = request.toRoom(user, questionType);
     return roomRepository.save(room);
   }
 }
