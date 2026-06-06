7410e1b473cf556f562f3de32b54d394f2d36e1e
NickyHariniai <ferrissushi@gmail.com>
2026-06-06 09:37:40 +0300

    chore: add tag for oas

--- src/main/java/com/ferrissushi/brainfart/endpoint/rest/controller/RoomController.java
+++ src/main/java/com/ferrissushi/brainfart/endpoint/rest/controller/RoomController.java
@@ -2,6 +2,9 @@ package com.ferrissushi.brainfart.endpoint.rest.controller;
 
 import com.ferrissushi.brainfart.model.dto.RoomDto;
 import com.ferrissushi.brainfart.service.RoomService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@Tag(name = "Room")
 public class RoomController {
   private final RoomService roomService;
 
