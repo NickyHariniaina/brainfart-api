539feb78f8b474285520a6f46b049fb80d3fff4a
NickyHariniai <ferrissushi@gmail.com>
2026-06-06 09:53:49 +0300

    chore: make dto mappinga accept user creator

--- src/main/java/com/ferrissushi/brainfart/model/dto/CreateRoomDto.java
+++ src/main/java/com/ferrissushi/brainfart/model/dto/CreateRoomDto.java
@@ -2,7 +2,10 @@ package com.ferrissushi.brainfart.model.dto;
 
 import com.ferrissushi.brainfart.model.QuestionType;
 import com.ferrissushi.brainfart.model.Room;
+import com.ferrissushi.brainfart.model.User;
+
 import java.time.Instant;
+import java.util.UUID;
 
 public record CreateRoomDto(
     String title,
@@ -10,13 +13,16 @@ public record CreateRoomDto(
     Integer chronoTiming,
     QuestionType questionType,
     Integer questionCount) {
-  public Room to() {
+  public Room toRoom(User creator) {
     return Room.builder()
         .title(this.title)
         .startDate(this.startDate)
         .chronoTiming(this.chronoTiming)
         .questionType(this.questionType)
         .questionCount(this.questionCount)
+        .openedBy(creator)
+        .id(UUID.randomUUID().toString())
+        .closed(false)
         .build();
   }
 }
