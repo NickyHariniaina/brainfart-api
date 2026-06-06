716a1c86ead2d7fd08f2f9bd4519657c3cffb2da
NickyHariniai <ferrissushi@gmail.com>
2026-06-06 09:57:39 +0300

    chore: pass default user as room creator

--- src/main/java/com/ferrissushi/brainfart/service/RoomService.java
+++ src/main/java/com/ferrissushi/brainfart/service/RoomService.java
@@ -1,7 +1,11 @@
 package com.ferrissushi.brainfart.service;
 
+import com.ferrissushi.brainfart.model.Room;
+import com.ferrissushi.brainfart.model.User;
+import com.ferrissushi.brainfart.model.dto.CreateRoomDto;
 import com.ferrissushi.brainfart.model.dto.RoomDto;
 import com.ferrissushi.brainfart.repository.RoomRepository;
+import com.ferrissushi.brainfart.repository.UserRepository;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +16,15 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class RoomService {
   private final RoomRepository roomRepository;
+  private final UserRepository userRepository;
 
   public List<RoomDto> findAll() {
     return roomRepository.findAll().stream().map(RoomDto::from).toList();
+  }
+
+  public Room Save(CreateRoomDto request) {
+    var user = userRepository.findByUsername("ferrissushi").orElse(User.builder().build());
+    var room = request.toRoom(user);
+    return roomRepository.save(room);
   }
 }
