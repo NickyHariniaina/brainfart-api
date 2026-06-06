cc3132c428446f2a22b6651af922c3be4867ab0f
NickyHariniai <ferrissushi@gmail.com>
2026-06-06 09:27:10 +0300

    style: format code

--- src/main/java/com/ferrissushi/brainfart/service/RoomService.java
+++ src/main/java/com/ferrissushi/brainfart/service/RoomService.java
@@ -2,11 +2,9 @@ package com.ferrissushi.brainfart.service;
 
 import com.ferrissushi.brainfart.model.dto.RoomDto;
 import com.ferrissushi.brainfart.repository.RoomRepository;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +14,6 @@ public class RoomService {
   private final RoomRepository roomRepository;
 
   public List<RoomDto> findAll() {
-      return roomRepository.findAll().stream().map(RoomDto::from).toList();
+    return roomRepository.findAll().stream().map(RoomDto::from).toList();
   }
 }
